7613331724a8380848f425afc3c991fcc80cd14b
Soul-Remix <soulremix-dev@pm.me>
2023-02-10 17:31:54 +0200

    Fix styling on larger screens

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,19 @@ import "./App.css";
 import NavBar from "./components/NavBar/NavBar";
 import TrackingInput from "@/components/TrackingInput/TrackingInput";
 import TrackingDetail from "@/components/TrackingDetail/TrackingDetail";
+import ActivityLog from "@/components/ActivityLog/ActivityLog";
 
 function App() {
-  return (
-    <div className="App">
-      <NavBar />
-        <TrackingInput/>
-        <TrackingDetail/>
-    </div>
-  );
+    return (
+        <div className="App">
+            <NavBar/>
+            <TrackingInput/>
+            <div className="details-container">
+                <TrackingDetail/>
+                <ActivityLog/>
+            </div>
+        </div>
+    );
 }
 
 export default App;
