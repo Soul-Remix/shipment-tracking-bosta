b3a73529fbb3ef58c0198b3ccca45519581a2572
Soul-Remix <soulremix-dev@pm.me>
2023-02-11 14:40:42 +0200

    fix mui not using cairo font

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,29 @@
 import "./App.css";
 import NavBar from "./components/NavBar/NavBar";
-import {queryClient} from "@/lib/react-query";
-import {QueryClientProvider} from "react-query";
+import { queryClient } from "@/lib/react-query";
+import { QueryClientProvider } from "react-query";
 import TrackingPage from "@/pages/TrackingPage";
-import relativeTime from 'dayjs/plugin/relativeTime';
+import relativeTime from "dayjs/plugin/relativeTime";
 import dayjs from "dayjs";
+import { createTheme, ThemeProvider } from "@mui/material";
 
-dayjs.extend(relativeTime)
+dayjs.extend(relativeTime);
+
+const theme = createTheme({
+  typography: {
+    fontFamily: ["cairo", "Roboto", "Arial", "sans-serif"].join(","),
+  },
+});
 
 function App() {
-
-    return (
-        <QueryClientProvider client={queryClient}>
-            <div>
-                <NavBar/>
-                <TrackingPage/>
-            </div>
-        </QueryClientProvider>
-    );
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <NavBar />
+        <TrackingPage />
+      </ThemeProvider>
+    </QueryClientProvider>
+  );
 }
 
 export default App;
