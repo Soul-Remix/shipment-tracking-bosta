4eaf3a4d968f8e8172def9f23c89f7067d69d1c0
Soul-Remix <soulremix-dev@pm.me>
2023-02-12 09:21:50 +0200

    Install testing libraries

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
+
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/shipment-tracking-bosta/",
   plugins: [react(), tsconfigPaths()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+  },
 });
